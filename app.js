eaa1b677a653df1579f70fe92b24ec9b5cc49733
babumungar <babumungar@gmail.com>
2021-08-05 08:33:43 +0000

    commit message

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const path = require("path");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const databasePath = path.join(__dirname, "userData.db");
-
+const databasePath = path.join(__dirname, "twitterClone.db");
 const app = express();
 
 app.use(express.json());
@@ -29,6 +29,28 @@ const initializeDbAndServer = async () => {
 };
 
 initializeDbAndServer();
+
+const authenticateToken = (request, response, next) => {
+  let jwtToken;
+  const authHeader = request.headers["authorization"];
+  if (authHeader !== undefined) {
+    jwtToken = authHeader.split(" ")[1];
+  }
+  if (jwtToken === undefined) {
+    response.status(401);
+    response.send("Invalid JWT Token");
+  } else {
+    jwt.verify(jwtToken, "MY_SECRET_TOKEN", async (error, payload) => {
+      if (error) {
+        response.send("Invalid JWT Token");
+      } else {
+        //console.log(payload);
+        request.username = payload.username;
+        next();
+      }
+    });
+  }
+};
 
 const validatePassword = (password) => {
   return password.length > 5;
@@ -63,4 +85,63 @@ app.post("/register/", async (request, response) => {
     response.send("User already exists");
   }
 });
+
+//User Login API
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbUser = await database.get(selectUserQuery);
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      const payload = {
+        username: username,
+      };
+      const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
+  }
+});
+
+//API3
+
+app.get("/user/tweets/feed/", authenticateToken, async (request, response) => {
+  const getTweetsQuery = `
+        SELECT
+            user.username,
+            tweet.tweet,
+            tweet.date_time as dateTime
+        FROM
+            user NATURAL JOIN tweet
+        ORDER BY
+            date_time DESC
+              
+        LIMIT     
+          4;`;
+
+  const tweetsArray = await database.all(getTweetsQuery);
+  response.send(tweetsArray);
+});
+
+//API4
+app.get("/user/following/", authenticateToken, async (request, response) => {
+  const getNamesQuery = `
+        SELECT
+            user.name
+        FROM
+            user INNER JOIN follower ON user.user_id = follower.follower_user_id
+        WHERE 
+            user.user_id = follower.following_user_id
+        GROUP BY 
+            name;`;
+  const namesArray = await database.all(getNamesQuery);
+  response.send(namesArray);
+});
+
 module.exports = app;
